c2eaf7b571c9c0f7f0d2f287d2bad11253d85c07
Sam Cook <samcookie9@gmail.com>
2016-10-04 00:00:26 -0400

    adding imac code

---
+++ lab3.java
@@ -0,0 +1,24 @@
+/*Complete the binary search tree class in BST.java by adding a delete method. 
+In the case in which the deleted node has two children, write the code in 
+two ways. 
+
+a) always replace the deleted node with the largest node on the left.
+b) count how many deletions have been done, and for even deletions
+replace the deleted node with the largest node on the left, for odd
+deletions, replace the deleted node with the smallest node on the right.
+
+Add a (recursive) method that returns the depth of the tree.
+
+Now do an experiment in which you add 100 random Doubles to the tree, then
+repeatedly delete and add values many times. (Keep an array of the numbers currently in the tree and randomly choose an element of the array to delete.)
+
+As you do each random deletion and insertion, note the depth of the tree, and
+create a graph (using Excel?) that shows how the tree's depth changes.
+
+Do this epxperiment for each of the deletion strategies a) and b).  How do
+the graphs differ?
+*/
+
+public class lab3.java{
+	public static void main
+}
